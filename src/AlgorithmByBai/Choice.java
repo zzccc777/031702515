bc3000d970d5db7bfd2d28ad69105011043af93e
zzccc777 <55140409+zzccc777@users.noreply.github.com>
2019-10-30 11:02:36 +0800

    Add files via upload

---
+++ src/AlgorithmByBai/Choice.java
@@ -0,0 +1,11 @@
+package AlgorithmByBai;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Choice {
+	public List<Card> head = new ArrayList<Card>();  
+	public List<Card> mid = new ArrayList<Card>();
+	public List<Card> end = new ArrayList<Card>();
+	String headType,midType,endType;
+}
